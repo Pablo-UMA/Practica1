7e9c35a35a6552481f5755918cc5c7fd294cf679
Pablo-UMA <pol93ronda@gmail.com>
2016-03-13 12:44:03 +0100

    modificaciones en el fichero Lib.java por Alumno1

--- Lib.java
+++ Lib.java
@@ -7,6 +7,7 @@ public class Lib {
 
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1"); //alumno1
     }
 
     public void metodoAlumno2(){
@@ -15,5 +16,6 @@ public class Lib {
 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        System.out.println("Alumno 1 inserta esta sentencia");
     }
 }
